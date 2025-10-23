704a179b4413fde7a69fcac183932fecc16653c0
Glib Shemenkov <shemenkovglib@gmail.com>
2025-10-23 23:13:21 +0200

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const create = async () => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+
+  try {
+    await fs.access(filePath);
+    throw new Error("Fs operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.writeFile(filePath, "I am fresh and young");
+      console.log("File created successfully");
+    } else {
+      throw err;
+    }
+  }
 };
 
 await create();
